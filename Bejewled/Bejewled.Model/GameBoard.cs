a709cde66b2e10937dbb8da701e4a85a0a2daf06
Ivan Tsaklev <tsaklev@gmail.com>
2016-06-15 17:59:41 +0300

    Added GetHint() method with hint logic
    # Conflicts:
    #	Bejewled/.vs/Bejewled/v14/.suo
    #	Bejewled/Bejewled.Model/bin/x86/Debug/Bejewled.Model.dll
    #	Bejewled/Bejewled.Model/bin/x86/Debug/Bejewled.Model.pdb
    
    #	Bejewled/Bejewled.Model/obj/x86/Debug/Bejewled.Core.csproj.FileListAbsolute.txt
    
    #	Bejewled/Bejewled.Model/obj/x86/Debug/Bejewled.Core.csprojResolveAssemblyReference.cache
    #	Bejewled/Bejewled.Model/obj/x86/Debug/Bejewled.Model.dll
    #	Bejewled/Bejewled.Model/obj/x86/Debug/Bejewled.Model.pdb
    
    #	Bejewled/Bejewled.Model/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    #	Bejewled/Bejewled.View/Bejewled.View/bin/x86/Debug/Bejewled.Model.dll
    #	Bejewled/Bejewled.View/Bejewled.View/bin/x86/Debug/Bejewled.Model.pdb
    #	Bejewled/Bejewled.View/Bejewled.View/bin/x86/Debug/Bejewled.View.exe
    #	Bejewled/Bejewled.View/Bejewled.View/bin/x86/Debug/Bejewled.View.pdb
    
    #	Bejewled/Bejewled.View/Bejewled.View/obj/x86/Debug/Bejewled.View.csproj.FileListAbsolute.txt
    
    #	Bejewled/Bejewled.View/Bejewled.View/obj/x86/Debug/Bejewled.View.csprojResolveAssemblyReference.cache
    #	Bejewled/Bejewled.View/Bejewled.View/obj/x86/Debug/Bejewled.View.exe
    #	Bejewled/Bejewled.View/Bejewled.View/obj/x86/Debug/Bejewled.View.pdb
    
    #	Bejewled/Bejewled.View/Bejewled.View/obj/x86/Debug/ContentPipeline-{59FB6C3E-E183-46E3-A02B-3DB914C166D9}.xml
    
    #	Bejewled/Bejewled.View/Bejewled.View/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    
    #	Bejewled/Bejewled.View/Bejewled.ViewContent/obj/x86/Debug/Bejewled.ViewContent.contentproj.FileListAbsolute.txt
    
    #	Bejewled/Bejewled.View/Bejewled.ViewContent/obj/x86/Debug/Bejewled.ViewContent.contentprojResolveAssemblyReference.cache
    
    #	Bejewled/Bejewled.View/Bejewled.ViewContent/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache

--- Bejewled/Bejewled.Model/GameBoard.cs
+++ Bejewled/Bejewled.Model/GameBoard.cs
@@ -1,7 +1,7 @@
 ﻿namespace Bejewled.Model
 {
     using System;
-
+    using System.Collections.Generic;
     using Bejewled.Model.Interfaces;
 
     public class GameBoard : IGameBoard
@@ -94,6 +94,155 @@
             // todo: add logic for swaping tiles
             this.CheckForMatch();
             throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITile> GetHint(GameBoard gameBoard)
+        {
+            // List that stores all the matches we find 
+            List<ITile> matches = new List<ITile>();
+
+
+            for (int row = 0; row < this.gameBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.gameBoard.GetLength(1); col++)
+                {
+                    ITile currentTile = this.gameBoard[row, col];
+                    ITile rightNeighbourTile = this.gameBoard[row, col + 1];
+                    ITile downNeighbourTile = this.gameBoard[row + 1, col];
+
+                    bool twoInRowMatch = currentTile.TileType.Equals(rightNeighbourTile.TileType);
+                    bool twoInColumnMatch = currentTile.TileType.Equals(downNeighbourTile.TileType);
+
+                    // Six horizontal cases for potential match
+                    // 1. Check for: * & * & & * * * case
+                    if (col > 1 && twoInRowMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row, col - 2].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(rightNeighbourTile);
+                        matches.Add(this.gameBoard[row, col - 2]);
+                    }
+
+                    // 2. Check for: * & & * & * * * case
+                    if (col < NumberOfColumn - 4 && twoInRowMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row, col + 3].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(rightNeighbourTile);
+                        matches.Add(this.gameBoard[row, col + 3]);
+                    }
+
+                    // 3. Check for case:
+                    // * & * * * *  
+                    // * * & & * * 
+                    if (col > 0 && row > 0 && twoInRowMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row - 1, col - 1].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(rightNeighbourTile);
+                        matches.Add(this.gameBoard[row - 1, col - 1]);
+                    }
+
+                    // 4. Check for case:
+                    // * * * * & *  
+                    // * * & & * * 
+                    if (col < NumberOfColumn - 2 && row > 0 && twoInRowMatch
+                        && currentTile.TileType.Equals(this.gameBoard[row - 1, col + 2].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(rightNeighbourTile);
+                        matches.Add(this.gameBoard[row - 1, col + 2]);
+                    }
+
+                    // 5. Check for case:
+                    // * * & & * *  
+                    // * & * * * *
+                    if (row < NumberOfRows - 2 && col > 0 && twoInRowMatch
+                        && currentTile.TileType.Equals(this.gameBoard[row + 1, col - 1].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(rightNeighbourTile);
+                        matches.Add(this.gameBoard[row + 1, col - 1]);
+                    }
+
+                    // 6. Check for case:
+                    // * * & & * *  
+                    // * * * * & * 
+                    if (row < NumberOfRows - 2 && col < NumberOfColumn - 2 && twoInRowMatch
+                        && currentTile.TileType.Equals(this.gameBoard[row + 1, col + 2].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(rightNeighbourTile);
+                        matches.Add(this.gameBoard[row + 1, col + 2]);
+                    }
+
+                    // Six vertical cases for potential match
+
+                    // case 1
+                    if (row < NumberOfRows - 3 && twoInColumnMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row + 3, col].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(downNeighbourTile);
+                        matches.Add(this.gameBoard[row + 3, col]);
+                    }
+
+                    // case 2
+                    if (row > 1 && twoInColumnMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row - 2, col].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(downNeighbourTile);
+                        matches.Add(this.gameBoard[row - 2, col]);
+                    }
+
+                    // case 3
+                    if (row > 0 && col > 0 && twoInColumnMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row - 1, col - 1].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(downNeighbourTile);
+                        matches.Add(this.gameBoard[row - 1, col - 1]);
+                    }
+                    
+                    // case 4
+                    if (row > 0 && col < NumberOfColumn - 2 && twoInColumnMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row - 1, col + 1].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(downNeighbourTile);
+                        matches.Add(this.gameBoard[row - 1, col + 1]);
+                    }
+
+                    // case 5
+                    if (col > 0 && row < NumberOfRows - 2 && twoInColumnMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row + 2, col - 1].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(downNeighbourTile);
+                        matches.Add(this.gameBoard[row + 2, col - 1]);
+                    }
+
+                    // case 6
+                    if (row < NumberOfRows - 2 && col < NumberOfColumn - 1 && twoInColumnMatch &&
+                        currentTile.TileType.Equals(this.gameBoard[row + 2, col + 1].TileType))
+                    {
+                        matches.Add(currentTile);
+                        matches.Add(downNeighbourTile);
+                        matches.Add(this.gameBoard[row + 2, col + 1]);
+                    }
+                }
+            }
+
+            if (matches.Count >= 3)
+            {
+                Random rand = new Random();
+                yield return matches[rand.Next(matches.Count - 1)];
+            }
+            else
+            {
+                yield return null;
+            }
         }
     }
 }
